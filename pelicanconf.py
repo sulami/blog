bfae6d686fa4e6f6c28fd4bc255de8b87d2cd23b
Robin Schroer <sulamiification@gmail.com>
2014-07-03 22:43:12 +0200

    pelicanconf: tweaked dates and feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,22 +3,12 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'sulami'
-SITENAME = u'sulami\'s blog'
+SITENAME = u'/dev/urandom > blog'
 SITEURL = 'https://sulami.github.io'
 
-TIMEZONE = 'Europe/Paris'
-
+TIMEZONE = 'Europe/Berlin'
+DEFAULT_DATE = (2012, 3, 2, 14, 1, 1)
 DEFAULT_LANG = u'en'
-
-# Blogroll
-# LINKS =  (('Pelican', 'http://getpelican.com/'),
-          # ('Python.org', 'http://python.org/'),
-          # ('Jinja2', 'http://jinja.pocoo.org/'),
-          # ('You can modify those links in your config file', '#'),)
-
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-          # ('Another social link', '#'),)
 
 THEME = '/home/sulami/build/pelican-theme'
 
@@ -31,11 +21,8 @@ MD_EXTENSIONS = (['codehilite(noclasses=true,pygments_style=friendly)'])
 
 STATIC_PATHS = ['images', 'download']
 
-GOOGLE_ANALYTICS = 'UA-38330260-4'
 RELATIVE_URLS = False
 FEED_DOMAIN = SITEURL
-FEED_ALL_ATOM = 'feed/atom.xml'
 FEED_ALL_RSS = 'feed/rss.xml'
-CATEGORY_FEED_ATOM = 'feed/%s.atom.xml'
 CATEGORY_FEED_RSS = 'feed/%s.rss.xml'
 

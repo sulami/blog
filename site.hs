98e812a8890d4f8b46bebd0dde09ed3448f24e8b
Robin Schroer <robin.schroer@ostmodern.co.uk>
2017-11-11 18:58:29 +0000

    Change the index page header

--- site.hs
+++ site.hs
@@ -61,7 +61,6 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
                     defaultContext
 
             getResourceBody

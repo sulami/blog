414748e27378d89f304db381432ee9b00701e272
Robin Schroer <git@sulami.xyz>
2024-02-19 10:46:48 +0900

    Make footnotes work, finally
    This was a pain to get working, mostly because HTML is annoying.

---
+++ input/content/about.md
@@ -0,0 +1,16 @@
+title = "About"
+kind = "page"
+---
+
+I'm a Robin Schroer, a human, located on the third planet, Sol system, Orion-Cygnus arm, Milky Way, Virgo supercluster.
+
+Sometimes I produce sequences of characters that can be meaningfully interpreted by humans and/or computers.
+
+## Contact / Links
+
+- Email: [blog@sulami.xyz](mailto:blog@sulami.xyz)
+- PGP: A1418106
+- Github: [sulami](https://github.com/sulami)
+- Lobsters: [sulami](https://lobste.rs/sulami)
+- Rands Leadership Slack: my name
+
--- src/main.rs
+++ src/main.rs
@@ -631,11 +631,12 @@ fn render_markdown(source: &str, site: &Site) -> String {
     let ss = &SS;
     let theme_set = &TS;
 
-    let parser = pulldown_cmark::Parser::new_ext(
+    let events = pulldown_cmark::Parser::new_ext(
         source,
         pulldown_cmark::Options::ENABLE_FOOTNOTES | pulldown_cmark::Options::ENABLE_TABLES,
     )
     .filter_map(|mut ev| match ev {
+        // Syntax highlighting for code blocks.
         Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(ref lang))) => {
             code_language = Some(lang.to_string());
             None
@@ -661,28 +662,86 @@ fn render_markdown(source: &str, site: &Site) -> String {
                 Some(ev)
             }
         }
-        // Event::FootnoteReference(ref label) => {
-        //     ev = Event::Html(
-        //         format!(
-        //             r##"<input type="checkbox" id="fnr{label}" /><label for="fnr{label}"><sup></sup></label>"##,
-        //         )
-        //         .into(),
-        //     );
-        //     Some(ev)
-        // }
-        // Event::Start(Tag::FootnoteDefinition(ref label)) => {
-        //     ev = Event::Html(
-        //         format!(
-        //             r##"<div class="footnote-definition" id="fnd{label}">"##,
-        //         )
-        //         .into(),
-        //     );
-        //     Some(ev)
-        // }
         _ => Some(ev),
+    })
+    .collect::<Vec<_>>();
+
+    // NB Inlining footnotes is a two-pass process because the definitions can be in any order, and
+    // potentially before the references(?). So we do a first pass where we collect all the
+    // definitions under their respective labels, and then do a second pass where we replace the
+    // references with the definitions and remove the original definitions.
+    //
+    // Another nuance here is that we strip paragrahps inside footnotes, because they are not HTML
+    // phrasing content, and thus not allowed inside <p>.
+
+    // Collect footnote definitions.
+    let mut footnotes: HashMap<pulldown_cmark::CowStr<'_>, Vec<Event>> = HashMap::default();
+    let mut current_footnote = None;
+
+    for ev in events.iter() {
+        match ev {
+            Event::Start(Tag::FootnoteDefinition(label)) => {
+                footnotes.insert(label.clone(), vec![]);
+                current_footnote = Some(label);
+            }
+            Event::End(TagEnd::FootnoteDefinition) => {
+                current_footnote = None;
+            }
+            Event::Start(Tag::Paragraph) if current_footnote.is_some() => {
+                // Don't collect paragraphs inside footnotes.
+            }
+            Event::End(TagEnd::Paragraph) if current_footnote.is_some() => {
+                // Don't collect paragraphs inside footnotes.
+            }
+            _ => {
+                if let Some(label) = current_footnote {
+                    footnotes.get_mut(label).unwrap().push(ev.clone());
+                }
+            }
+        }
+    }
+
+    // Strip out footnote definitions and patch footnote references with them.
+    let mut in_footnote = false;
+    let events = events.into_iter().filter_map(|ev| match ev {
+        Event::Start(Tag::FootnoteDefinition(_)) => {
+            in_footnote = true;
+            None
+        }
+        Event::End(TagEnd::FootnoteDefinition) => {
+            in_footnote = false;
+            None
+        }
+        Event::FootnoteReference(label) => {
+            // Render just the definition of the footnote and re-insert it into the stream as an
+            // HTML event.
+            let mut definition = footnotes
+                .get_mut(&label)
+                .expect("footnote without definition found")
+                .clone();
+            definition.insert(
+                0,
+                Event::Html(
+                    format!(
+                        // r#"<details class="footnote"><summary><sup>{label}</sup></summary><div class="footnote-definition">"#
+                        r#"<input type="checkbox" id="cb-{label}" /><label for="cb-{label}"><sup>{label}</sup></label><span class="footnote">"#
+                    )
+                    .into(),
+                ),
+            );
+            // <input type="checkbox" id="cb1" /><label for="cb1"><sup></sup></label><span><br><br>This is the footnote text.<br><br></span>
+
+            // definition.push(Event::Html("</div></details>".into()));
+            definition.push(Event::Html("</span>".into()));
+            let mut rendered_footnote = String::new();
+            pulldown_cmark::html::push_html(&mut rendered_footnote, definition.into_iter());
+            Some(Event::Html(rendered_footnote.into()))
+        }
+        _ if in_footnote => None,
+        ev => Some(ev),
     });
 
-    pulldown_cmark::html::push_html(&mut rendered, parser);
+    pulldown_cmark::html::push_html(&mut rendered, events);
 
     rendered
 }
